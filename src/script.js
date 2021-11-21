87fd638f49949d2bc2562340993a76cd3607af55
Hayleigh Winpenny <hayleigh.winpenny@outlook.com>
2021-11-21 23:32:48 +0000

    >0 form

--- src/script.js
+++ src/script.js
@@ -2,9 +2,9 @@ function submitForm() {
   const name = document.querySelector('#firstname');
   const email = document.querySelector('#emailaddress');
 
-  if (name.value === '' && email.value.length > 1){
+  if (name.value === '' && email.value.length > 0){
     alert("Please enter your name.");
-  } else if (email.value === '' && name.value.length > 1){
+  } else if (email.value === '' && name.value.length > 0){
     alert("Please enter your email address.");
   } else if (name.value === '' && email.value === '') {
     alert("Please enter your details.")
