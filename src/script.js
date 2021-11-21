31fe4f8f15de87df28ab6205cdf9c39eae6e4b5f
Hayleigh Winpenny <hayleigh.winpenny@outlook.com>
2021-11-21 23:30:56 +0000

    Contact Form working!

--- src/script.js
+++ src/script.js
@@ -1,6 +1,14 @@
-//form
+function submitForm() {
+  const name = document.querySelector('#firstname');
+  const email = document.querySelector('#emailaddress');
 
-function SubmitForm(){
-    
-   alert("Thank you! I'll be in touch.")
-    }
+  if (name.value === '' && email.value.length > 1){
+    alert("Please enter your name.");
+  } else if (email.value === '' && name.value.length > 1){
+    alert("Please enter your email address.");
+  } else if (name.value === '' && email.value === '') {
+    alert("Please enter your details.")
+  } else {
+      alert("Thank you! I'll be in touch soon!")
+  }
+}
